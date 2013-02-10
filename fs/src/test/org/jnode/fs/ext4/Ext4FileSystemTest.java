b1e7f60f6d41feb7fafdd6a38eee4c5ec9f43b83
Luke Quinane <luke.quinane@gmail.com>
2013-02-11 09:26:29 +1100

    Add a test for ext4 file systems.

---
+++ fs/src/test/org/jnode/fs/ext4/Ext4FileSystemTest.java
@@ -0,0 +1,45 @@
+package org.jnode.fs.ext4;
+
+import junit.framework.TestCase;
+import org.jnode.driver.Device;
+import org.jnode.driver.block.FileDevice;
+import org.jnode.fs.DataStructureAsserts;
+import org.jnode.fs.FileSystemTestUtils;
+import org.jnode.fs.ext2.Ext2FileSystem;
+import org.jnode.fs.ext2.Ext2FileSystemType;
+import org.jnode.fs.service.FileSystemService;
+
+public class Ext4FileSystemTest extends TestCase {
+
+    private Device device;
+    private FileSystemService fss;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        // create file system service.
+        fss = FileSystemTestUtils.createFSService(Ext2FileSystemType.class.getName());
+    }
+
+    public void testReadExt4SpecialFiles() throws Exception {
+
+        device = new FileDevice(FileSystemTestUtils.getTestFile("ext4/test-special-files.ext4"), "r");
+        Ext2FileSystemType type = fss.getFileSystemType(Ext2FileSystemType.ID);
+        Ext2FileSystem fs = type.create(device, true);
+
+        String expectedStructure =
+            "type: EXT2 vol: total:15728640 free:13918208\n" +
+                "  /; \n" +
+                "    lost+found; \n" +
+                "    console; 0; d41d8cd98f00b204e9800998ecf8427e\n" +
+                "    fifo; 0; d41d8cd98f00b204e9800998ecf8427e\n" +
+                "    sda1; 0; d41d8cd98f00b204e9800998ecf8427e\n" +
+                "    The_Rabies_Virus_Remains_a_Medical_Mystery.jpg; 6606; a28d342db2d2081f9d2eb287d49c1110\n" +
+                "    wired-science.jpg; 16; 67bc4bf64a29239a9f148fb768bfbbc8\n" +
+                "    wolf_slice_1.jpg; 6606; a28d342db2d2081f9d2eb287d49c1110\n" +
+                "    index.html; 106102; 99248bc850c65b03b04776342e4b3e7d\n";
+
+        DataStructureAsserts.assertStructure(fs, expectedStructure);
+    }
+}
+
